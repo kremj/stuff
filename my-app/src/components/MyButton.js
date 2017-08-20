dd902c848fb0add219e187ed4cbe0e86d13a4bfd
Burak Mete <bcanpinar@havelsan.com.tr>
2017-08-20 21:54:59 +0300

    reformatting lines

--- my-app/src/components/MyButton.js
+++ my-app/src/components/MyButton.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 export default class MyButton extends React.Component {
     render() {
-        return(
+        return (
             <button>
                 Click me
             </button>
